79b46ccd5525637bf7479d58cdc635de067857a9
yuviguvi <50619688+yuviguvi@users.noreply.github.com>
2020-08-21 20:02:25 +0530

    Add files via upload

--- task 1/src/App.js
+++ task 1/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import Main from "./component/Main";
+import New from "./component/New";
+import post from "./component/post";
+import "./App.css";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  let history = useHistory();
+  return(
+   <Router history={history}>
+     <div>
+       <Switch>
+         <Route path='/' exact component={Main} />
+         <Route path='/New/:key' component={New}/>  `                        m                                             , jn bj`
+       </Switch>
+     </div>
+   </Router>
   );
 }
-
 export default App;
